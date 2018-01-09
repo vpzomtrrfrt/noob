32e7dc7c5924aec39a9d2b471e958636a0987648
Colin Reeder <vpzomtrrfrt@gmail.com>
2018-01-08 17:47:33 -0700

    More errors

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -9,8 +9,10 @@ use futures::prelude::*;
 use std::str::FromStr;
 
 pub enum Error {
-    HTTP(hyper::Error),
-    TLS(native_tls::Error)
+    HTTPError(hyper::Error),
+    TLSError(native_tls::Error),
+    AuthenticationFailed,
+    UnexpectedResponse(String)
 }
 
 impl std::fmt::Debug for Error {
@@ -28,8 +30,10 @@ impl std::fmt::Display for Error {
 impl std::error::Error for Error {
     fn description(&self) -> &str {
         match *self {
-            Error::HTTP(ref err) => std::error::Error::description(err),
-            Error::TLS(ref err) => std::error::Error::description(err)
+            Error::HTTPError(ref err) => std::error::Error::description(err),
+            Error::TLSError(ref err) => std::error::Error::description(err),
+            Error::AuthenticationFailed => "Authentication Failed",
+            Error::UnexpectedResponse(ref msg) => &msg
         }
     }
 }
@@ -43,16 +47,19 @@ impl Client {
     pub fn login_bot(handle: &tokio_core::reactor::Handle, token: &str) -> Box<Future<Item=Client, Error=Error>> {
         let handle = handle.clone();
         let http = hyper::Client::configure()
-            .connector(fut_try!(hyper_tls::HttpsConnector::new(1, &handle).map_err(|e|Error::TLS(e))))
+            .connector(fut_try!(hyper_tls::HttpsConnector::new(1, &handle).map_err(|e|Error::TLSError(e))))
             .build(&handle);
         let mut request = hyper::Request::new(
             hyper::Method::Get,
-            fut_try!(hyper::Uri::from_str("https://discordapp.com/api/gateway/bot").map_err(|e|Error::HTTP(e.into())))
+            fut_try!(hyper::Uri::from_str("https://discordapp.com/api/gateway/bot").map_err(|e|Error::HTTPError(e.into())))
             );
         Box::new(http.request(request)
-                 .map_err(|e|Error::HTTP(e))
+                 .map_err(|e|Error::HTTPError(e))
            .and_then(|response| {
                 println!("{:?}", response);
+                if response.status() == hyper::StatusCode::Unauthorized {
+                    return futures::future::err(Error::AuthenticationFailed);
+                }
                 futures::future::ok(Client {
                     handle,
                     http
