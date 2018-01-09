5866990c8daf55cccd50030e69c7b08b1923f0be
Colin Reeder <vpzomtrrfrt@gmail.com>
2018-01-08 19:26:11 -0700

    Working HTTP authentication

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -3,6 +3,7 @@ use hyper;
 use hyper_tls;
 use native_tls;
 use futures;
+use serde_json;
 use std;
 
 use futures::prelude::*;
@@ -38,6 +39,29 @@ impl std::error::Error for Error {
     }
 }
 
+#[derive(Debug, Clone)]
+struct BotAuthorizationScheme {
+    token: String
+}
+
+impl FromStr for BotAuthorizationScheme {
+    type Err = Error;
+    fn from_str(token: &str) -> Result<Self, Self::Err> {
+        Ok(BotAuthorizationScheme {
+            token: token.to_owned()
+        })
+    }
+}
+
+impl hyper::header::Scheme for BotAuthorizationScheme {
+    fn scheme() -> Option<&'static str> {
+        Some("Bot")
+    }
+    fn fmt_scheme(&self, fmt: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
+        write!(fmt, "{}", self.token)
+    }
+}
+
 pub struct Client {
     handle: tokio_core::reactor::Handle,
     http: hyper::Client<hyper_tls::HttpsConnector<hyper::client::HttpConnector>>
@@ -51,19 +75,35 @@ impl Client {
             .build(&handle);
         let mut request = hyper::Request::new(
             hyper::Method::Get,
-            fut_try!(hyper::Uri::from_str("https://discordapp.com/api/gateway/bot").map_err(|e|Error::HTTPError(e.into())))
+            fut_try!(hyper::Uri::from_str("https://discordapp.com/api/v6/gateway/bot").map_err(|e|Error::HTTPError(e.into())))
             );
+        request.headers_mut().set(
+            hyper::header::Authorization(
+                fut_try!(BotAuthorizationScheme::from_str(token))));
         Box::new(http.request(request)
                  .map_err(|e|Error::HTTPError(e))
            .and_then(|response| {
                 println!("{:?}", response);
-                if response.status() == hyper::StatusCode::Unauthorized {
-                    return futures::future::err(Error::AuthenticationFailed);
+                let status = response.status();
+                if status == hyper::StatusCode::Unauthorized {
+                    return Err(Error::AuthenticationFailed);
                 }
-                futures::future::ok(Client {
-                    handle,
-                    http
-                })
-            }))
+                if status == hyper::StatusCode::Ok {
+                    return Ok(response.body());
+                }
+                Err(Error::UnexpectedResponse(format!("Gateway request responded with unexpected status {}", status)))
+           })
+           .and_then(|body| body.concat2().map_err(|e|Error::HTTPError(e)))
+           .and_then(|chunk| {
+               let value: serde_json::Value = match serde_json::from_slice(&chunk) {
+                   Ok(value) => value,
+                   Err(err) => return futures::future::err(Error::UnexpectedResponse(format!("Gateway request responded with invalid JSON: {:?}", err)))
+               };
+               println!("{}", value);
+               futures::future::ok(Client {
+                   handle,
+                   http
+               })
+           }))
     }
 }
