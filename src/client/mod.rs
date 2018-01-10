1995c7fcda3b868dfedbd207029cff2dc206c533
Colin Reeder <vpzomtrrfrt@gmail.com>
2018-01-09 22:17:25 -0700

    change things randomly

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -15,7 +15,7 @@ pub enum Error {
     TLSError(native_tls::Error),
     WebsocketError(websocket::WebSocketError),
     AuthenticationFailed,
-    UnexpectedResponse(String)
+    UnexpectedResponse(String),
 }
 
 impl std::fmt::Debug for Error {
@@ -37,7 +37,7 @@ impl std::error::Error for Error {
             Error::TLSError(ref err) => std::error::Error::description(err),
             Error::WebsocketError(ref err) => std::error::Error::description(err),
             Error::AuthenticationFailed => "Authentication Failed",
-            Error::UnexpectedResponse(ref msg) => &msg
+            Error::UnexpectedResponse(ref msg) => &msg,
         }
     }
 }
@@ -49,19 +49,19 @@ enum ConnectionState {
     Connecting,
     Connected(WebSocket),
     Ready(WebSocket),
-    Failed(Error)
+    Failed(Error),
 }
 
 #[derive(Debug, Clone)]
 struct BotAuthorizationScheme {
-    token: String
+    token: String,
 }
 
 impl FromStr for BotAuthorizationScheme {
     type Err = Error;
     fn from_str(token: &str) -> Result<Self, Self::Err> {
         Ok(BotAuthorizationScheme {
-            token: token.to_owned()
+            token: token.to_owned(),
         })
     }
 }
@@ -78,78 +78,116 @@ impl hyper::header::Scheme for BotAuthorizationScheme {
 pub struct Client {
     handle: tokio_core::reactor::Handle,
     gateway_url: String,
-    connection: ConnectionState
+    connection: ConnectionState,
 }
 
 impl Client {
-    pub fn login_bot(handle: &tokio_core::reactor::Handle, token: &str) -> Box<Future<Item=Client, Error=Error>> {
+    pub fn login_bot(
+        handle: &tokio_core::reactor::Handle,
+        token: &str,
+    ) -> Box<Future<Item = Client, Error = Error>> {
         let handle = handle.clone();
         let http = hyper::Client::configure()
-            .connector(fut_try!(hyper_tls::HttpsConnector::new(1, &handle).map_err(|e|Error::TLSError(e))))
+            .connector(fut_try!(
+                hyper_tls::HttpsConnector::new(1, &handle).map_err(|e| Error::TLSError(e))
+            ))
             .build(&handle);
         let mut request = hyper::Request::new(
             hyper::Method::Get,
-            fut_try!(hyper::Uri::from_str("https://discordapp.com/api/v6/gateway/bot").map_err(|e|Error::HTTPError(e.into())))
-            );
-        request.headers_mut().set(
-            hyper::header::Authorization(
-                fut_try!(BotAuthorizationScheme::from_str(token))));
-        Box::new(http.request(request)
-                 .map_err(|e|Error::HTTPError(e))
-           .and_then(|response| {
-                println!("{:?}", response);
-                let status = response.status();
-                if status == hyper::StatusCode::Unauthorized {
-                    return Err(Error::AuthenticationFailed);
-                }
-                if status == hyper::StatusCode::Ok {
-                    return Ok(response.body());
-                }
-                Err(Error::UnexpectedResponse(format!("Gateway request responded with unexpected status {}", status)))
-           })
-           .and_then(|body| body.concat2().map_err(|e|Error::HTTPError(e)))
-           .and_then(|chunk| -> Box<futures::future::Future<Item=Client, Error=Error>> {
-               let value: serde_json::Value = match serde_json::from_slice(&chunk) {
-                   Ok(value) => value,
-                   Err(err) => return Box::new(futures::future::err(
-                       Error::UnexpectedResponse(
-                           format!(
-                               "Unable to parse gateway API response: {:?}", err))))
-               };
-               println!("{}", value);
-               let client = Client::new(handle, value["url"].to_string());
-               Box::new(client.connect()
-                   .map(|_|client))
-           }))
+            fut_try!(
+                hyper::Uri::from_str("https://discordapp.com/api/v6/gateway/bot")
+                    .map_err(|e| Error::HTTPError(e.into()))
+            ),
+        );
+        request.headers_mut().set(hyper::header::Authorization(
+            fut_try!(BotAuthorizationScheme::from_str(token)),
+        ));
+        Box::new(
+            http.request(request)
+                .map_err(|e| Error::HTTPError(e))
+                .and_then(|response| {
+                    println!("{:?}", response);
+                    let status = response.status();
+                    if status == hyper::StatusCode::Unauthorized {
+                        return Err(Error::AuthenticationFailed);
+                    }
+                    if status == hyper::StatusCode::Ok {
+                        return Ok(response.body());
+                    }
+                    Err(Error::UnexpectedResponse(format!(
+                        "Gateway request responded with unexpected status {}",
+                        status
+                    )))
+                })
+                .and_then(|body| body.concat2().map_err(|e| Error::HTTPError(e)))
+                .and_then(
+                    |chunk| -> Box<futures::future::Future<Item = Client, Error = Error>> {
+                        let value: serde_json::Value = match serde_json::from_slice(&chunk) {
+                            Ok(value) => value,
+                            Err(err) => {
+                                return Box::new(futures::future::err(Error::UnexpectedResponse(
+                                    format!("Unable to parse gateway API response: {:?}", err),
+                                )))
+                            }
+                        };
+                        println!("{}", value);
+                        let client = Client::new(handle, value["url"].to_string());
+                        Box::new(client.connect().map(|_| client))
+                    },
+                ),
+        )
     }
     fn new(handle: tokio_core::reactor::Handle, gateway_url: String) -> Self {
         Client {
             handle,
             gateway_url,
-            connection: ConnectionState::Disconnected
+            connection: ConnectionState::Disconnected,
         }
     }
-    fn connect(&mut self) -> Box<futures::future::Future<Item=(), Error=Error>> {
+    fn connect(&mut self) -> Box<futures::future::Future<Item = (), Error = Error>> {
         if match self.connection {
             ConnectionState::Disconnected => true,
             ConnectionState::Connecting => false,
             ConnectionState::Connected(_) => return Box::new(futures::future::ok(())),
-            ConnectionState::Failed(_) => true
+            ConnectionState::Failed(_) => true,
         } {
             self.connection = ConnectionState::Connecting;
-            self.handle.spawn(fut_try!(
-                    websocket::ClientBuilder::new(&format!("{}?v=6&encoding=json", self.gateway_url))
-                    .map_err(|err| Error::UnexpectedResponse(
-                            format!("Unable to parse gateway URI: {}", err)
-                            )))
-                .async_connect(None, &self.handle)
+            let builder = match websocket::ClientBuilder::new(
+                &format!("{}?v=6&encoding=json", self.gateway_url),
+            ) {
+                Ok(builder) => builder,
+                Err(err) => {
+                    return Box::new(futures::future::err(Error::UnexpectedResponse(
+                        format!("Unable to parse gateway URI: {}", err),
+                    )))
+                }
+            };
+            let connection: websocket::client::async::ClientNew<Box<websocket::async::Stream + Send>> = builder.async_connect(None, &self.handle);
+            let task = connection
                 .and_then(|(socket, _)| {
-                    self.handle.spawn(socket.for_each(|packet|self.handle_packet(packet)).map_err(|e|Error::WebsocketError(e)));
-                }));
+                    let task = socket
+                            .and_then(|packet| {
+                                match self.handle_packet(packet) {
+                                    Ok(_) => Ok(()),
+                                    Err(err) => {
+                                        panic!("Unable to deal with the packet: {}", err);
+                                    }
+                                }
+                            })
+                            .map_err(|_|());
+                    self.handle.spawn(
+                        task
+                    );
+                    futures::future::ok(())
+                })
+                .map_err(|_|());
+            self.handle.spawn(task);
         }
-        Box::new(futures::future::err(Error::UnexpectedResponse("TODO make this work".to_owned())))
+        Box::new(futures::future::err(
+            Error::UnexpectedResponse("TODO make this work".to_owned()),
+        ))
     }
-    fn handle_packet(&mut self, message: websocket::message::OwnedMessage) -> Result<(), Error> {
+    fn handle_packet(&mut self, message: websocket::message::OwnedMessage) -> Result<(), websocket::WebSocketError> {
         println!("{:?}", message);
         Ok(())
     }
