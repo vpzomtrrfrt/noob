f46017b834a4877a4fbd3f4caf9cf4371fb5333a
Colin Reeder <vpzomtrrfrt@gmail.com>
2018-03-10 15:49:49 -0700

    Message sending/receiving

--- src/events.rs
+++ src/events.rs
@@ -1,20 +1,21 @@
 #[derive(Clone, PartialEq, Debug)]
 pub enum Event<'a> {
-    Ready(ReadyData<'a>)
+    Ready(ReadyData<'a>),
+    MessageCreate(Message)
 }
 
 pub type Snowflake = String;
 
 #[derive(Deserialize, Debug, PartialEq, Clone)]
 pub struct User {
-    id: Snowflake,
-    username: String,
-    discriminator: String,
-    avatar: Option<String>,
-    bot: Option<bool>,
-    mfa_enabled: Option<bool>,
-    verified: Option<bool>,
-    email: Option<String>
+    pub id: Snowflake,
+    pub username: String,
+    pub discriminator: String,
+    pub avatar: Option<String>,
+    pub bot: Option<bool>,
+    pub mfa_enabled: Option<bool>,
+    pub verified: Option<bool>,
+    pub email: Option<String>
 }
 
 #[derive(Debug, PartialEq, Clone)]
@@ -22,4 +23,13 @@ pub struct ReadyData<'a> {
     pub guilds: &'a [Snowflake],
     pub session_id: &'a str,
     pub user: User
+}
+
+#[derive(Debug, PartialEq, Clone, Deserialize)]
+pub struct Message {
+    pub id: Snowflake,
+    pub channel_id: Snowflake,
+    pub author: User,
+    pub content: String,
+    pub timestamp: String
 }
