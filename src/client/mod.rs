702256951879430f0e133fe7395196c665f47468
Colin Reeder <vpzomtrrfrt@gmail.com>
2018-07-05 17:22:24 -0600

    Undo removing impl
    Apparently it fixed the docs, but broke normal compilation.  The latter
    seems more important :p

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -21,7 +21,7 @@ pub struct Client {
 impl Client {
     pub fn connect(
         token: &str,
-    ) -> Box<Future<Item = (Client, Stream<Item = Event, Error = Error>), Error = Error> + Send>
+    ) -> Box<Future<Item = (Client, impl Stream<Item = Event, Error = Error>), Error = Error> + Send>
     {
         let http =
             hyper::Client::builder().build(try_future_box!(hyper_tls::HttpsConnector::new(1)));
