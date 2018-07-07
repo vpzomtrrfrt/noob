5a87fa3577aa150bcef8198305c6822d55cbe0ea
Colin Reeder <vpzomtrrfrt@gmail.com>
2018-07-07 17:05:02 -0600

    Reduce use of boxes

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -6,7 +6,7 @@ use websocket;
 
 use Error;
 
-use futures::{Future, Stream};
+use futures::{Future, IntoFuture, Stream};
 
 mod stream;
 
@@ -20,20 +20,22 @@ impl Client {
     /// Connect to the Discord gateway with a bot token
     pub fn connect(
         token: &str,
-    ) -> Box<Future<Item = (Client, stream::GatewayConnection), Error = Error> + Send> {
-        let http =
-            hyper::Client::builder().build(try_future_box!(hyper_tls::HttpsConnector::new(1)));
-
-        let gateway_req = try_future_box!(
-            hyper::Request::get("https://discordapp.com/api/v6/gateway/bot")
-                .header(hyper::header::AUTHORIZATION, token)
-                .body(Default::default())
-                .map_err(|e| Error::Other(format!("{:?}", e)))
-        );
-
+        ) -> impl Future<Item = (Client, stream::GatewayConnection), Error = Error> + Send {
         let token = token.to_owned();
-
-        Box::new(
+        {
+            let token_ref: &str = &token;
+            hyper_tls::HttpsConnector::new(1)
+                .map(|connector| hyper::Client::builder().build(connector))
+                .map_err(|e| e.into())
+                .into_future()
+                .join(
+                    hyper::Request::get("https://discordapp.com/api/v6/gateway/bot")
+                    .header(hyper::header::AUTHORIZATION, token_ref)
+                    .body(Default::default())
+                    .map_err(|e| Error::Other(format!("{:?}", e)))
+                    )
+        }
+        .and_then(|(http, gateway_req)| {
             http.request(gateway_req)
                 .map_err(|e| e.into())
                 .and_then(
@@ -46,12 +48,12 @@ impl Client {
                                 Box::new(resp.into_body().concat2().map_err(|e| e.into()))
                             }
                             status => Box::new(futures::future::err(Error::Other(format!(
-                                "Gateway request returned unexpected status {}",
-                                status
-                            )))),
+                                            "Gateway request returned unexpected status {}",
+                                            status
+                                            )))),
                         }
                     },
-                )
+                    )
                 .and_then(|body| {
                     #[derive(Deserialize)]
                     struct GetGatewayResult<'a> {
@@ -71,10 +73,10 @@ impl Client {
                                     token: token.clone(),
                                 },
                                 stream::GatewayConnection::connect_new(url, token),
-                            )
+                                )
                         })
-                }),
-        )
+                })
+        })
     }
 
     /// Send a message on a channel
@@ -82,38 +84,38 @@ impl Client {
         &self,
         message: &::MessageBuilder,
         channel: &str,
-    ) -> Box<Future<Item = (), Error = Error> + Send> {
-        let body = try_future_box!(message.to_request_body(channel));
-        let auth_value = format!("Bot {}", self.token);
-        let auth_value_ref: &str = &auth_value;
-        let req = try_future_box!(
-            hyper::Request::post(format!(
-                "https://discordapp.com/api/v6/channels/{}/messages",
-                channel
-            )).header(hyper::header::AUTHORIZATION, auth_value_ref)
-                .header(hyper::header::CONTENT_TYPE, "application/json")
-                .header(hyper::header::CONTENT_LENGTH, body.len())
-                .body(body.into())
-                .map_err(|e| Error::Other(format!("Failed to create request: {:?}", e)))
-        );
-
-        Box::new(
-            self.http_client
-                .request(req)
-                .map_err(|e| e.into())
-                .and_then(|resp| -> Box<Future<Item = (), Error = Error> + Send> {
-                    match resp.status() {
-                        hyper::StatusCode::OK => Box::new(futures::future::ok(())),
-                        _ => Box::new(resp.into_body().concat2().map_err(|e| e.into()).and_then(
+        ) -> impl Future<Item = (), Error = Error> + Send {
+        message.to_request_body(channel)
+            .and_then(|body| {
+                let auth_value = format!("Bot {}", self.token);
+                let auth_value_ref: &str = &auth_value;
+                hyper::Request::post(format!(
+                        "https://discordapp.com/api/v6/channels/{}/messages",
+                        channel
+                        )).header(hyper::header::AUTHORIZATION, auth_value_ref)
+                    .header(hyper::header::CONTENT_TYPE, "application/json")
+                    .header(hyper::header::CONTENT_LENGTH, body.len())
+                    .body(body.into())
+                    .map_err(|e| Error::Other(format!("Failed to create request: {:?}", e)))
+            })
+        .and_then(|req| {
+            Ok(self.http_client.request(req)
+               .map_err(|e| e.into()))
+        })
+        .into_future()
+            .and_then(|x| x)
+            .and_then(|resp| -> Box<Future<Item = (), Error = Error> + Send> {
+                match resp.status() {
+                    hyper::StatusCode::OK => Box::new(futures::future::ok(())),
+                    _ => Box::new(resp.into_body().concat2().map_err(|e| e.into()).and_then(
                             |body| {
                                 Err(Error::Other(format!(
-                                    "Message sending failed {}",
-                                    String::from_utf8_lossy(&body.to_vec())
-                                )))
+                                            "Message sending failed {}",
+                                            String::from_utf8_lossy(&body.to_vec())
+                                            )))
                             },
-                        )),
-                    }
-                }),
-        )
+                            )),
+                }
+            })
     }
 }
