a4dd171a8b2a6cf6ee5181774ac51ef27003ff17
Colin Reeder <colin@vpzom.click>
2020-06-13 09:52:27 -0600

    Update connect example to new tokio

--- examples/connect.rs
+++ examples/connect.rs
@@ -1,19 +1,12 @@
-extern crate futures;
-extern crate noob;
-extern crate tokio_core;
+use futures::{TryStreamExt};
 
-use futures::{Future, Stream};
-
-fn main() {
-    let mut core = tokio_core::reactor::Core::new().unwrap();
-    let handle = core.handle();
+#[tokio::main]
+async fn main() {
     let token = std::env::var("DISCORD_TOKEN").expect("Missing DISCORD_TOKEN");
 
-    let task = noob::Client::connect(&token).and_then(|(client, stream)| {
-        stream.for_each(|evt| {
-            println!("{:?}", evt);
-            Ok(())
-        })
-    });
-    core.run(task).unwrap();
+    let (_client, stream) = noob::Client::connect(&token).await.expect("Failed to connect to Discord");
+    stream.try_for_each(|evt| {
+        println!("{:?}", evt);
+        futures::future::ready(Ok(()))
+    }).await.unwrap();
 }
